20ad28843af11a27e02adc1e02a19777b49a9282
Mostafa7amedFront <mostafahamed3003@gmail.com>
2025-12-26 22:45:49 +0200

    navbar Done

--- src/app/Layout/navbar/navbar.ts
+++ src/app/Layout/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoginService } from '../../Core/service/login';
+import { ReactiveModeuls } from '../../Shared/Modules/ReactiveForms.module';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule  , ReactiveModeuls],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
@@ -12,7 +13,11 @@ export class Navbar {
   _login = inject(LoginService);
 
 
+isMenuOpen = false;
 
+toggleMenu() {
+  this.isMenuOpen = !this.isMenuOpen;
+}
      logout() {
     this._login.logout();
   }
