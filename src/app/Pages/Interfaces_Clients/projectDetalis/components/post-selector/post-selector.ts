d41794aaea175f900db82e02be22f2d667f4f737
Mostafa7amedFront <mostafahamed3003@gmail.com>
2026-01-09 22:30:10 +0200

    done

--- src/app/Pages/Interfaces_Clients/projectDetalis/components/post-selector/post-selector.ts
+++ src/app/Pages/Interfaces_Clients/projectDetalis/components/post-selector/post-selector.ts
@@ -7,27 +7,21 @@ declare var __Animations: any;
   templateUrl: './post-selector.html',
   styleUrl: './post-selector.scss'
 })
-export class PostSelector  implements AfterViewInit {
+export class PostSelector implements AfterViewInit {
   anim: any;
 
-ngAfterViewInit() {
-  this.anim = OnlineWebFonts_Com({
-    Id: '.my-anim',
-    Data: __Animations['520251'],
-    Status: (progress: any, id: any) => {
-    }
-  });
+  ngAfterViewInit() {
+    this.anim = OnlineWebFonts_Com({
+      Id: '.my-anim',
+      Data: __Animations['520251'],
+      Status: (progress: any, id: any) => {}
+    });
 
-  // ⭐ NOW the element exists → Play works
-  this.anim.Play();
-}
-
-  ngOnInit() {
-     this.anim.Play();
+    this.anim.Play(); // ✔️ صح
   }
 
-  play() { this.anim.Play(); }
-  pause() { this.anim.Pause(); }
-  resume() { this.anim.Resume(); }
-  stop() { this.anim.Stop(); }
+  play()   { this.anim?.Play(); }
+  pause()  { this.anim?.Pause(); }
+  resume() { this.anim?.Resume(); }
+  stop()   { this.anim?.Stop(); }
 }
